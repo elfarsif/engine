e1d52192c6217412e4f04978f7bd3673f578a97a
Frank El Farsi <f.n.elfarsi@gmail.com>
2025-11-12 17:46:20 -0500

    Initial commit : libgdx gradle project from scratch with json java dependency added

---
+++ src/main/java/io/github/elfarsif/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package io.github.elfarsif;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+    public static void main() {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("My GDX Game");
+        config.setWindowedMode(800, 480);
+        config.useVsync(true);
+        new Lwjgl3Application(new Game(), config);
+    }
+}
